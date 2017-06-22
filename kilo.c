554a5cd520da301ea0004e68dee03b0d561ed072
Connor Ruggles <cruggles@iastate.edu>
2017-06-21 20:59:44 -0500

    follow more steps

--- kilo.c
+++ kilo.c
@@ -1,7 +1,35 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disable_raw_mode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enable_raw_mode() {
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disable_raw_mode);;
+
+	struct termios raw = orig_termios;
+	raw.c_lflag &= ~(ECHO | ICANON);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
+	enable_raw_mode();
+
 	char c;
-	while(read(STDIN_FILENO, &c, 1) == 1);
+	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if(iscntrl(c)) {
+			printf("%d\n", c);
+		} else {
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 	return 0;
 }
