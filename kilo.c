4dbe4e9809c27ba8053e3a49bf64f65b9c37e772
Connor Ruggles <cruggles@iastate.edu>
2017-06-23 17:15:39 -0500

    more steps

--- kilo.c
+++ kilo.c
@@ -3,8 +3,14 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <errno.h>
 
 struct termios orig_termios;
+
+void die(const char *s) {
+	perror(s);
+	exit(1);
+}
 
 void disable_raw_mode() {
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
@@ -15,7 +21,12 @@ void enable_raw_mode() {
 	atexit(disable_raw_mode);;
 
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -24,11 +35,16 @@ int main() {
 	enable_raw_mode();
 
 	char c;
-	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+	while(1) {
+		c = '\0';
+		read(STDIN_FILENO, &c, 1);
 		if(iscntrl(c)) {
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		} else {
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
+		}
+		if(c == 'q') {
+			break;
 		}
 	}
 	return 0;
